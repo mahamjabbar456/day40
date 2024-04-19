862a4e673da6f13598720128570e1e10aaf13b50
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-04-19 17:30:10 +0500

    i learn about for loop,while loop and for of loop.

---
+++ index.js
@@ -0,0 +1,24 @@
+// Question 118
+for (let num = 1; num < 11; num++) {
+    console.log(num);
+}
+// Question 119
+let num = 1;
+while (num < 6) {
+    console.log("Hello World");
+    num++;
+}
+// Question 120
+// let countries = new Map<string,string>();
+// countries.set("Pakistan","Islamabad");
+// countries.set("France","Paris");
+// countries.set("Saudia Arabia","Riydha");
+// for (const capital of countries) {
+//     console.log(`Country is ${capital[0]} and capital is ${capital[1]}`);
+// }
+let names = ["Taimoor", "Daniyal", "Jabbar", "Areeb", "Tanveer"];
+for (const name of names) {
+    console.log(name);
+}
+export {};
+i learn about for loop,while loop and for of loop.
---
+++ index.ts
@@ -0,0 +1,24 @@
+// Question 118
+for(let num:number = 1;num<11;num++){
+    console.log(num);
+}
+
+// Question 119
+let num:number = 1;
+while (num<6) {
+    console.log("Hello World");
+    num++;
+}
+
+// Question 120
+// let countries = new Map<string,string>();
+// countries.set("Pakistan","Islamabad");
+// countries.set("France","Paris");
+// countries.set("Saudia Arabia","Riydha");
+// for (const capital of countries) {
+//     console.log(`Country is ${capital[0]} and capital is ${capital[1]}`);
+// }
+let names:string[] = ["Taimoor","Daniyal","Jabbar","Areeb","Tanveer"];
+for (const name of names) {
+    console.log(name);
+}
